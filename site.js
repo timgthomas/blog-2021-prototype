f96b5d51b4636d105eb77b9e5a4e7028885b55a7
Tim G. Thomas <tim@timgthomas.com>
2021-08-04 15:23:08 -0500

    add focus highlighting

--- site.js
+++ site.js
@@ -1,13 +1,18 @@
+function highlight(e, highlightType) {
+  if (!e.target.matches('[data-section]')) return
+  let section = e.target.getAttribute('data-section')
+  banner.classList.add(`${highlightType}-${section}`)
+}
+
+function unhighlight(e, highlightType) {
+  if (!e.target.matches('[data-section]')) return
+  let section = e.target.getAttribute('data-section')
+  banner.classList.remove(`${highlightType}-${section}`)
+}
+
 let banner = document.querySelector('[role="banner"]')
 
-banner.addEventListener('mouseover', (e) => {
-  if (!e.target.matches('[data-tint]')) return
-  let tint = e.target.getAttribute('data-tint')
-  banner.style.setProperty('--color-accent', `var(--palette-${tint}`)
-  banner.style.setProperty('--color-accent-accessible', `var(--palette-${tint}-accessible`)
-})
-
-banner.addEventListener('mouseout', () => {
-  banner.style.setProperty('--color-accent', null)
-  banner.style.setProperty('--color-accent-accessible', null)
-})
+banner.addEventListener('focusin', (e) => highlight(e, 'focus'))
+banner.addEventListener('focusout', (e) => unhighlight(e, 'focus'))
+banner.addEventListener('mouseover', (e) => highlight(e, 'hover'))
+banner.addEventListener('mouseout', (e) => unhighlight(e, 'hover'))
